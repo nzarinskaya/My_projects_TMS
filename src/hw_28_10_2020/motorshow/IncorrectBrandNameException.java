2dd977b7d9b0ac47de3665de0ad2e7bbfeffa040
MxmStm <you@example.com>
2020-10-28 22:23:26 +0300

    add commit hw 28_10_2020

---
+++ src/hw_28_10_2020/motorshow/IncorrectBrandNameException.java
@@ -0,0 +1,7 @@
+package hw_28_10_2020.motorshow;
+
+public class IncorrectBrandNameException extends Exception {
+    public IncorrectBrandNameException(String message) {
+        super(message);
+    }
+}
